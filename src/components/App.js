5f6684626f3a2cbb1b6501e0e4919d8f6d4d35c0
Ellie Kemp <elliekemp95@gmail.com>
2021-09-27 19:55:50 +0100

    Axios request moved to app.js, autocomplete search bar functionality added

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,39 @@
 import React, { useEffect, useState } from "react";
-import "../styles/App.css";
+import axios from "axios";
+import "../styles/App.scss";
 import SearchForm from "./SearchForm";
 import AirQualitySummary from "./AirQualitySummary";
-import getAirQuality from "../requests/getAirQuality";
 
 const App = () => {
-  const [searchText, setSearchText] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [summary, setSummary] = useState(undefined);
-
-  const handleCitySearch = () => {
-    getAirQuality(searchText, setSummary, setErrorMessage);
-  };
+  const [results, setResults] = useState([]);
 
   useEffect(() => {
-    getAirQuality(searchText, setSummary, setErrorMessage);
+    axios
+      .request({
+        method: "GET",
+        url: "https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/latest?limit=747&page=1&offset=0&sort=desc&radius=1000&country_id=GB&order_by=lastUpdated&dumpRaw=false",
+      })
+      .then((response) => {
+        setResults(response.data.results);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
+
+  console.log(results);
 
   return (
     <div className="App">
-      <h1>Air Quality App</h1>
-      <SearchForm
-        searchText={searchText}
-        setSearchText={setSearchText}
-        onSubmit={handleCitySearch}
-      />
-      <AirQualitySummary summary={summary} errorMessage={errorMessage} />
+      <h1 className="app-title">Compare your Air</h1>
+      <p className="app-subtitle1">
+        Compare the air quality between cities in the UK.
+      </p>
+      <p className="app-subtitle2">
+        Select cities to compare using the search tool below.
+      </p>
+      <SearchForm results={results} />
+      <AirQualitySummary />
     </div>
   );
 };
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,12 +1,56 @@
-import React from "react";
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+
+import React, { useState } from "react";
 import PropTypes from "prop-types";
+import "../styles/SearchForm.scss";
 
-const SearchForm = ({ searchText, setSearchText, onSubmit }) => {
-  const handleInputChange = (event) => setSearchText(event.target.value);
+const SearchForm = ({ results, onSubmit }) => {
+  const [searchText, setSearchText] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
 
+  const onChangeHandler = (text) => {
+    let matches = [];
+    if (text.length > 0) {
+      const cities = results.filter((result) => result.city != null);
+      matches = cities.filter((result) => {
+        const regex = new RegExp(`${text}`, "gi");
+        return result.city.match(regex);
+      });
+    }
+    console.log("matches", matches);
+    setSuggestions(matches);
+    setSearchText(text);
+  };
+
+  const onSuggestHandler = (text) => {
+    setSearchText(text);
+    setSuggestions([]);
+  };
   return (
-    <div className="search-form">
-      <input type="text" onChange={handleInputChange} value={searchText} />
+    <div className="container">
+      <input
+        type="text"
+        onChange={(e) => onChangeHandler(e.target.value)}
+        value={searchText}
+        className="input-box"
+        onBlur={() => {
+          setTimeout(() => {
+            setSuggestions([]);
+          }, 100);
+        }}
+      />
+      {suggestions &&
+        suggestions.map((suggestion, i) => (
+          <div
+            key={i}
+            className="suggestion"
+            onClick={() => onSuggestHandler(suggestion.location)}
+          >
+            {suggestion.location}, {suggestion.city}
+          </div>
+        ))}
       <button type="submit" onClick={onSubmit} className="search-form__button">
         Search
       </button>
@@ -17,7 +61,20 @@ const SearchForm = ({ searchText, setSearchText, onSubmit }) => {
 export default SearchForm;
 
 SearchForm.propTypes = {
-  searchText: PropTypes.string.isRequired,
-  setSearchText: PropTypes.func.isRequired,
+  results: PropTypes.arrayOf(
+    PropTypes.shape({
+      city: PropTypes.string.isRequired,
+      location: PropTypes.string.isRequired,
+      country: PropTypes.string.isRequired,
+      measurements: PropTypes.arrayOf(
+        PropTypes.shape({
+          lastUpdated: PropTypes.string.isRequired,
+        }).isRequired
+      ).isRequired,
+    }).isRequired
+  ).isRequired,
+
+  // searchText: PropTypes.string.isRequired,
+  // setSearchText: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired,
 };
